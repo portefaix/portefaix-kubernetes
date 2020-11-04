35ea8818c6b06f879682a474d4d254cf9521a88d
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2020-11-04 10:21:27 +0100

    Add: Sops on GCP
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ iac/gcp/sops/terraform/main.tf
@@ -0,0 +1,30 @@
+# Copyright (C) 2020 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "google_kms_key_ring" "sops" {
+  name     = format("%s_sops", var.project)
+  location = var.key_ring_location
+}
+
+resource "google_kms_crypto_key" "sops" {
+  name     = format("%s_sops", var.project)
+  key_ring = google_kms_key_ring.sops.id
+  purpose  = "ENCRYPT_DECRYPT"
+
+  labels = var.crypto_key_labels
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
