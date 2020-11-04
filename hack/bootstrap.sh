18da6b5835f6cf7025e59fa0184ae92e9e82b575
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2020-11-04 13:48:51 +0100

    Add: init hack scripts
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ hack/bootstrap.sh
@@ -0,0 +1,28 @@
+# Copyright (C) 2020 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+NO_COLOR="\033[0m"
+DEBUG_COLOR="\e[34m"
+INFO_COLOR="\e[32m"
+ERROR_COLOR="\e[31m"
+WARN_COLOR="\e[35m"
+
+# Check Flux v2 prerequisites
+flux check --pre
+[[ $? -ne 0 ]] && echo "Prerequisites were not satisfied" && exit 1
+
+if [[ -f .secrets/k8s-secret-sealed-secret-private-key.yaml ]]; then
+  echo "Applying existing sealed-secret key"
+  kubectl apply -f .secrets/k8s-secret-sealed-secret-private-key.yaml
+fi
