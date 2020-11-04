775d35b5fc17489c9307ac09f12f1b38227db11f
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2020-11-04 13:34:43 +0100

    Add: load credentials
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ portefaix.sh
@@ -0,0 +1,33 @@
+# Copyright (C) 2020 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+OK_COLOR="\e[32m"
+KO_COLOR="\e[31m"
+NO_COLOR="\e[39m"
+
+echo -e "${OK_COLOR}[ Portefaix / lab ]${NO_COLOR}"
+
+echo -e "${OK_COLOR}Setup credentials ${NO_COLOR}"
+CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
+PORTEFAIX_CONFIG_HOME=${CONFIG_HOME}/portefaix
+
+export LAB_CREDENTIALS=${PORTEFAIX_CONFIG_HOME}/lab.sh
+
+if [ ! -f ${LAB_CREDENTIALS} ]; then
+    echo -e "${KO_COLOR}File not found: ${NO_COLOR}${LAB_CREDENTIALS}"
+else
+    . ${LAB_CREDENTIALS}
+    echo -e "${OK_COLOR}Done${NO_COLOR}"
+fi
+
