f68c9c762446ad05e8637380ff26bec1ec92fa0b
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-01-08 13:54:12 +0100

    Add: Alicloud makefiles
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ hack/alicloud.staging.mk
@@ -0,0 +1,22 @@
+# Copyright (C) 2020 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ALICLOUD_PROJECT_staging = portefaix-staging
+
+# https://www.alibabacloud.com/help/doc-detail/40654.htm
+ALICLOUD_REGION_staging = eu-central-1
+
+ALICLOUD_CLUSTER_staging = portefaix-staging-ack
+
+KUBE_CONTEXT_staging = 
