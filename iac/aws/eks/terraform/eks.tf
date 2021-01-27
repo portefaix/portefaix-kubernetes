069ae66526844eacb34adf753b329326fcf7b606
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-01-27 15:59:12 +0100

    Update: EKS module version
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- iac/aws/eks/terraform/eks.tf
+++ iac/aws/eks/terraform/eks.tf
@@ -13,9 +13,8 @@
 # limitations under the License.
 
 module "eks" {
-  #source  = "nlamirault/eks/aws"
-  #version = "0.6.0"
-  source = "/home/nicolas/Projects/terraform-aws-eks"
+  source  = "nlamirault/eks/aws"
+  version = "0.6.0"
 
   cluster_name       = var.cluster_name
   kubernetes_version = var.kubernetes_version
