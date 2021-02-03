3614dd2a81abe218426ea67a1ea938a019f2efc1
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-02-03 15:45:25 +0100

    Observability module v3.4.0 and IAM (#172)
    * Add: IAM service account user
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Update: use v3.4.0 of Observabiity module
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- iac/gcp/observability/terraform/main.tf
+++ iac/gcp/observability/terraform/main.tf
@@ -14,7 +14,7 @@
 
 module "prometheus" {
   source  = "nlamirault/observability/google//modules/prometheus"
-  version = "3.3.0"
+  version = "3.4.0"
 
   project = var.project
 
@@ -24,7 +24,7 @@ module "prometheus" {
 
 module "thanos" {
   source  = "nlamirault/observability/google//modules/thanos"
-  version = "3.3.0"
+  version = "3.4.0"
 
   project = var.project
 
@@ -40,7 +40,7 @@ module "thanos" {
 
 module "loki" {
   source  = "nlamirault/observability/google//modules/loki"
-  version = "3.3.0"
+  version = "3.4.0"
 
   project = var.project
 
@@ -56,7 +56,7 @@ module "loki" {
 
 module "tempo" {
   source  = "nlamirault/observability/google//modules/tempo/"
-  version = "3.3.0"
+  version = "3.4.0"
 
   project = var.project
 
@@ -72,7 +72,7 @@ module "tempo" {
 
 module "grafana" {
   source  = "nlamirault/observability/google//modules/grafana/"
-  version = "3.3.0"
+  version = "3.4.0"
   
   project = var.project
 
