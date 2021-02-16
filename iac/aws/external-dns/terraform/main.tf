00c15f0512d4869f6e5cd385d50259e0f3e1b53a
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-02-16 21:44:23 +0100

    Update: Terraform External DNS module
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- iac/aws/external-dns/terraform/main.tf
+++ iac/aws/external-dns/terraform/main.tf
@@ -12,6 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-locals {
-  service_name = format("%s-external-dns", var.cluster_name)
+module "external_dns" {
+  source  = "nlamirault/external-dns/aws"
+  version = "0.2.0"
+ 
+  cluster_name = var.cluster_name
+
+  namespace       = var.namespace
+  service_account = var.service_account
+
+  tags = var.tags
 }
--- iac/aws/external-dns/terraform/output.tf
+++ iac/aws/external-dns/terraform/output.tf
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+# Copyright (C) 2021 Nicolas Lamirault <nicolas.lamirault@gmail.com>
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-output "role_arn" {
-  value = element(aws_iam_role.external_dns.*.arn, 0)
+output "prometheus_role_arn" {
+  description = "Role ARN for Prometheus"
+  value       = module.external_dns.role_arn
 }
