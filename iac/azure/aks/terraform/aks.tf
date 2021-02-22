155edbfcd405ccea4153d0cc55f1e31f2d6acc92
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-02-22 08:42:59 +0100

    Update: use AKS Terraform module v0.8.0
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- iac/azure/aks/terraform/aks.tf
+++ iac/azure/aks/terraform/aks.tf
@@ -13,9 +13,8 @@
 # limitations under the License.
 
 module "aks" {
-  # source  = "nlamirault/aks/azurerm"
-  # version = "0.8.0"
-  source = "/home/nicolas/Projects/terraform-azurerm-aks"
+  source  = "nlamirault/aks/azurerm"
+  version = "0.8.0"
 
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
