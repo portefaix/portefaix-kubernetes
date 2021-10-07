e32dbe5d30c22f8d4c6589d4637cdda5f839fb03
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-10-07 11:00:39 +0200

    Update: use Age for Sops on Azure and AWS
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- hack/azure.dev.mk
+++ hack/azure.dev.mk
@@ -20,5 +20,8 @@ AZ_LOCATION_dev = francecentral
 
 CLUSTER_dev = portefaix-dev-aks
 
-SOPS_PROVIDER_dev = azure-kv
-SOPS_KEY_dev = https://portefaix-dev-sops-tmp.vault.azure.net/keys/portefaix-dev-sops-tmp/d4da1ad6b9cf474095774d06871c5c1b
+# SOPS_PROVIDER_dev = azure-kv
+# SOPS_KEY_dev = https://portefaix-dev-sops-tmp.vault.azure.net/keys/portefaix-dev-sops-tmp/d4da1ad6b9cf474095774d06871c5c1b
+SOPS_PROVIDER_dev = age
+SOPS_KEY_dev = age1wt7t48m206gv74llh9fs8u4kaapnjzhxydyk3a9mavzkaulsz5nq9qujgc
+SOPS_AGE_KEY_FILE_dev = .secrets/$(CLOUD)/$(ENV)/age/age.agekey
