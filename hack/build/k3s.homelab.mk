3a314cfc662e30efedee573258558f5ca9e5bd71
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-10-08 17:33:59 +0200

    Fix: Homelab with Age
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- hack/build/k3s.homelab.mk
+++ hack/build/k3s.homelab.mk
@@ -16,8 +16,11 @@ CLUSTER_homelab = k3s-portefaix-homelab
 
 KUBE_CONTEXT_homelab = k3s-portefaix-homelab
 
-SOPS_PROVIDER_homelab = pgp
-SOPS_KEY_homelab = 82515583D0225FABE4F4FC7874420B8A43DCF894
+# SOPS_PROVIDER_homelab = pgp
+# SOPS_KEY_homelab = 82515583D0225FABE4F4FC7874420B8A43DCF894
+SOPS_PROVIDER_homelab = age
+SOPS_KEY_homelab = age133ly60ep0tp9vm4t95a6c6wgvv9uqtpzsgwkw8wfneshvank79ysmew3wl
+SOPS_AGE_KEY_FILE_homelab = .secrets/$(CLOUD)/$(ENV)/age/age.agekey
 
 # Default variables
 MNT_DEVICE_homelab ?= /dev/mmcblk0
