2d9041fc3056cd878ce1cfc21bd1a56b7f4f8e86
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-10-28 16:05:11 +0200

    Add: Enable Cloud IAP API
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- hack/build/gcp.mk
+++ hack/build/gcp.mk
@@ -71,6 +71,7 @@ gcp-enable-apis: guard-ENV ## Enable APIs on project
 	gcloud services enable cloudresourcemanager.googleapis.com --project $(GCP_PROJECT)
 	gcloud services enable dns.googleapis.com --project $(GCP_PROJECT)
 	gcloud services enable cloudkms.googleapis.com --project $(GCP_PROJECT)
+	gcloud services enable iap.googleapis.com --project $(GCP_PROJECT)
 
 .PHONY: gcp-terraform-sa
 gcp-terraform-sa: guard-ENV ## Create service account for Terraform (ENV=xxx)
