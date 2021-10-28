cd186ab0efe07f9ee7e206acd53933a41c3ff5fc
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-10-28 16:21:33 +0200

    Update: Bastion IAP ssh connection
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- hack/build/gcp.mk
+++ hack/build/gcp.mk
@@ -143,7 +143,8 @@ gcp-kube-credentials: guard-ENV ## Generate credentials
 .PHONY: gcp-ssh-bastion
 gcp-ssh-bastion: guard-ENV ## SSH into the bastion through IAP
 	@echo -e "$(INFO_COLOR)Connect to the bastion for $(GCP_PROJECT)$(NO_COLOR)"
-	gcloud compute ssh $(GCP_BASTION) --tunnel-through-iap --project $(GCP_PROJECT) --zone $(GCP_BASTION_ZONE)
+	gcloud beta compute ssh $(GCP_BASTION) --tunnel-through-iap --project $(GCP_PROJECT) --zone $(GCP_BASTION_ZONE) -- -L8888:127.0.0.1:8888
+
 
 # ====================================
 # I N S P E C
