5b01f40f3199b8cfd918f3e5f68b8eab04cd93ee
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-11-16 15:18:04 +0100

    Fix: do not use module
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/aws/alb/modules/alb/sg.tf
+++ terraform/aws/alb/modules/alb/sg.tf
@@ -15,113 +15,123 @@
 # External
 
 #tfsec:ignore:AWS006
-module "external_http_80" {
-  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
-  version = "4.4.0"
+# module "external_http_80" {
+#   source  = "terraform-aws-modules/security-group/aws//modules/http-80"
+#   version = "4.4.0"
 
-  name        = local.alb_external_name
-  description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = data.aws_vpc.this.id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  tags = merge({
-    "Name" = local.alb_external_name
-    "Role" = "internal",
-  }, var.alb_tags)
-}
-
-module "external_https_443" {
-  source  = "terraform-aws-modules/security-group/aws//modules/https-443"
-  version = "4.4.0"
-
-  name        = local.alb_external_name
-  description = "Security group for web-server with HTTPS ports open within VPC"
-  vpc_id      = data.aws_vpc.this.id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  tags = merge({
-    "Name" = local.alb_internal_name
-    "Role" = "internal",
-  }, var.alb_tags)
-}
-
-# resource "aws_security_group" "alb_external" {
 #   name        = local.alb_external_name
-#   description = "Allow all HTTP and HTTPS inbound traffic"
+#   description = "Security group for web-server with HTTP ports open within VPC"
+#   vpc_id      = data.aws_vpc.this.id
 
-#   vpc_id = data.aws_vpc.this.id
-
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+#   ingress_cidr_blocks = ["0.0.0.0/0"]
 
 #   tags = merge({
 #     "Name" = local.alb_external_name
-#     "Role" = "external",
+#     "Role" = "internal",
 #   }, var.alb_tags)
-
-#   lifecycle {
-#     create_before_destroy = "true"
-#     ignore_changes        = [ingress]
-#   }
 # }
 
-# resource "aws_security_group_rule" "allow_http_external" {
-#   type              = "ingress"
-#   from_port         = 80
-#   to_port           = 80
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = aws_security_group.alb_external.id
-# }
+# module "external_https_443" {
+#   source  = "terraform-aws-modules/security-group/aws//modules/https-443"
+#   version = "4.4.0"
 
-# resource "aws_security_group_rule" "allow_https_external" {
-#   type              = "ingress"
-#   from_port         = 443
-#   to_port           = 443
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = aws_security_group.alb_external.id
-# }
+#   name        = local.alb_external_name
+#   description = "Security group for web-server with HTTPS ports open within VPC"
+#   vpc_id      = data.aws_vpc.this.id
 
-# # Internal
-
-# resource "aws_security_group" "alb_internal" {
-#   name        = local.alb_internal_name
-#   description = "Allow all HTTP and HTTPS inbound traffic from VPC"
-
-#   vpc_id = data.aws_vpc.this.id
-
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+#   ingress_cidr_blocks = ["0.0.0.0/0"]
 
 #   tags = merge({
 #     "Name" = local.alb_internal_name
 #     "Role" = "internal",
 #   }, var.alb_tags)
-
-#   lifecycle {
-#     create_before_destroy = "true"
-#     ignore_changes        = [ingress]
-#   }
 # }
 
-# resource "aws_security_group_rule" "allow_http_internal" {
-#   type              = "ingress"
-#   from_port         = 80
-#   to_port           = 80
-#   protocol          = "tcp"
-#   cidr_blocks       = ["10.0.0.0/8"]
-#   security_group_id = aws_security_group.alb_internal.id
-# }
+resource "aws_security_group" "alb_external" {
+  name        = local.alb_external_name
+  description = "Allow all HTTP and HTTPS inbound traffic"
+
+  vpc_id = data.aws_vpc.this.id
+
+  #tfsec:ignore:AWS018
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    #tfsec:ignore:AWS009
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge({
+    "Name" = local.alb_external_name
+    "Role" = "external",
+  }, var.alb_tags)
+
+  lifecycle {
+    create_before_destroy = "true"
+    ignore_changes        = [ingress]
+  }
+}
+
+resource "aws_security_group_rule" "allow_http_external" {
+  type        = "ingress"
+  description = "Allow HTTP inbound traffic"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  #tfsec:ignore:AWS006
+  #tfsec:ignore:AWS009
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_external.id
+}
+
+resource "aws_security_group_rule" "allow_https_external" {
+  type        = "ingress"
+  description = "Allow HTTPS inbound traffic"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  #tfsec:ignore:AWS009
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb_external.id
+}
+
+# Internal
+
+resource "aws_security_group" "alb_internal" {
+  name        = local.alb_internal_name
+  description = "Allow all HTTP and HTTPS inbound traffic from VPC"
+
+  vpc_id = data.aws_vpc.this.id
+
+  #tfsec:ignore:AWS018
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    #tfsec:ignore:AWS009
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge({
+    "Name" = local.alb_internal_name
+    "Role" = "internal",
+  }, var.alb_tags)
+
+  lifecycle {
+    create_before_destroy = "true"
+    ignore_changes        = [ingress]
+  }
+}
+
+resource "aws_security_group_rule" "allow_http_internal" {
+  type              = "ingress"
+  description       = "Allow HTTP inbound traffic"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/8"]
+  security_group_id = aws_security_group.alb_internal.id
+}
