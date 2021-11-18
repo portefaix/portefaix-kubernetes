ecbd92bdca753196cb788310d6c4a40b6d143497
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-11-18 18:32:19 +0100

    Add: Cert-Manager refactoring
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/gcp/cert-manager/modules/cert-manager/main.tf
+++ terraform/gcp/cert-manager/modules/cert-manager/main.tf
@@ -15,11 +15,10 @@
 terraform {
   required_version = ">= 1.0.0"
 
-  required_providers {
-    # tflint-ignore: terraform_unused_required_providers
-    google = {
-      source  = "hashicorp/google"
-      version = "< 4.0.0"
-    }
-  }
+  # required_providers {
+  #   google = {
+  #     source  = "hashicorp/google"
+  #     version = "< 4.0.0"
+  #   }
+  # }
 }
