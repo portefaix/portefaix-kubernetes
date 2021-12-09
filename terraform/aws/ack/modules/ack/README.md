e3ffaef0e5b615ab274d735737f927f41126806c
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-12-09 15:29:28 +0100

    Add: ACK on AWS
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ terraform/aws/ack/modules/ack/README.md
@@ -0,0 +1,37 @@
+## Documentation
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.0.0 |
+
+## Providers
+
+No providers.
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_ack"></a> [ack](#module\_ack) | /home/nicolas/Projects/terraform-aws-ack | n/a |
+
+## Resources
+
+No resources.
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_ack_eks_name"></a> [ack\_eks\_name](#input\_ack\_eks\_name) | ACK EKS controller name | `string` | `"eks"` | no |
+| <a name="input_cluster_name"></a> [cluster\_name](#input\_cluster\_name) | Name of the EKS cluster | `string` | n/a | yes |
+| <a name="input_namespace"></a> [namespace](#input\_namespace) | The K8s namespace for ACK Controller resources | `string` | `"ack-system"` | no |
+| <a name="input_services"></a> [services](#input\_services) | ACK services deployed | <pre>list(object({<br>    name       = string<br>    policy_arn = string<br>  }))</pre> | <pre>[<br>  {<br>    "name": "apigatewayv2",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonAPIGatewayAdministrator"<br>  },<br>  {<br>    "name": "applicationautoscaling",<br>    "policy_arn": "arn:aws:iam::aws:policy/PowerUserAccess"<br>  },<br>  {<br>    "name": "dynamodb",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"<br>  },<br>  {<br>    "name": "ec2",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonEC2FullAccess"<br>  },<br>  {<br>    "name": "ecr",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"<br>  },<br>  {<br>    "name": "apigatewayv2",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonAPIGatewayAdministrator"<br>  },<br>  {<br>    "name": "elasticache",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonElastiCacheFullAccess"<br>  },<br>  {<br>    "name": "kms",<br>    "policy_arn": "arn:aws:iam::aws:policy/AWSKeyManagementServicePowerUser"<br>  },<br>  {<br>    "name": "lambda",<br>    "policy_arn": "arn:aws:iam::aws:policy/AWSLambda_FullAccess"<br>  },<br>  {<br>    "name": "mq",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonMQApiFullAccess"<br>  },<br>  {<br>    "name": "opensearchservice",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonOpenSearchServiceFullAccess"<br>  },<br>  {<br>    "name": "rds",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonRDSFullAccess"<br>  },<br>  {<br>    "name": "s3",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonS3FullAccess"<br>  },<br>  {<br>    "name": "sagemaker",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"<br>  },<br>  {<br>    "name": "sfn",<br>    "policy_arn": "arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess"<br>  },<br>  {<br>    "name": "sns",<br>    "policy_arn": "arn:aws:iam::aws:policy/AmazonSNSFullAccess"<br>  }<br>]</pre> | no |
+| <a name="input_tags"></a> [tags](#input\_tags) | A map of tags to add to all resources | `map(string)` | n/a | yes |
+
+## Outputs
+
+No outputs.
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ terraform/aws/ack/staging/variables.tf
@@ -0,0 +1,136 @@
+# Copyright (C) 2021 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#############################################################################
+# Provider
+
+variable "region" {
+  type        = string
+  description = "AWS Region"
+}
+
+variable "default_tags" {
+  type        = map(string)
+  description = "Tags for the AWS provider"
+  default = {
+    "Project" = "portefaix"
+    "Made-By" = "terraform"
+  }
+}
+
+#############################################################################
+# ACK Controller
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the EKS cluster"
+}
+
+variable "namespace" {
+  description = "The K8s namespace for ALB Controller resources"
+  type        = string
+  default     = "ack-system"
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources."
+  type        = map(string)
+  default = {
+    "Made-By" = "terraform"
+  }
+}
+
+variable "ack_eks_name" {
+  type        = string
+  description = "ACK EKS controller name"
+  default     = "eks"
+}
+
+variable "services" {
+  description = "ACK services deployed"
+  type = list(object({
+    name       = string
+    policy_arn = string
+  }))
+  default = [
+    {
+      name       = "apigatewayv2"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonAPIGatewayAdministrator"
+    },
+    {
+      name       = "applicationautoscaling"
+      policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+    },
+    {
+      name       = "dynamodb"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+    },
+    {
+      name       = "ec2"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+    },
+    {
+      name       = "ecr"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+    },
+    # {
+    #   name       = "eks"
+    #   policy_arn = "arn:aws:iam::aws:policy/"
+    # },
+    {
+      name       = "apigatewayv2"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonAPIGatewayAdministrator"
+    },
+    {
+      name       = "elasticache"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonElastiCacheFullAccess"
+    },
+    {
+      name       = "kms"
+      policy_arn = "arn:aws:iam::aws:policy/AWSKeyManagementServicePowerUser"
+    },
+    {
+      name       = "lambda"
+      policy_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
+    },
+    {
+      name       = "mq"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonMQApiFullAccess"
+    },
+    {
+      name       = "opensearchservice"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonOpenSearchServiceFullAccess"
+    },
+    {
+      name       = "rds"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+    },
+    {
+      name       = "s3"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+    },
+    {
+      name       = "sagemaker"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+    },
+    {
+      name       = "sfn"
+      policy_arn = "arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess"
+    },
+    {
+      name       = "sns"
+      policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+    },
+  ]
+}
