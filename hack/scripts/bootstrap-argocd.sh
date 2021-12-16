57244c09755b9f51c8d2077ec1d3c829c2a7e28c
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2021-12-16 12:45:27 +0100

    Fix: infra apps
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- hack/scripts/bootstrap-argocd.sh
+++ hack/scripts/bootstrap-argocd.sh
@@ -19,7 +19,7 @@ color_red="\\e[31m"
 color_green="\\e[32m"
 color_blue="\\e[36m";
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 function echo_fail { echo -e "${color_red}✖ $*${reset_color}"; }
 function echo_success { echo -e "${color_green}✔ $*${reset_color}"; }
@@ -37,7 +37,7 @@ ARGOCD_VERSION="v2.1.7"
 # ARGOCD_NOTIFS_VERSION="1.6.0"
 # ARGO_ROLLOUTS="2.6.0"
 
-PROM_OPERATOR_VERSION="v0.52.1"
+# PROM_OPERATOR_VERSION="v0.52.1"
 
 CLOUD=$1
 [ -z "${CLOUD}" ] && echo_fail "Cloud provider not satisfied" && exit 1
@@ -56,39 +56,39 @@ function argocd_manifests() {
     local version=$1
 
     kubectl create namespace "${ARGOCD_NAMESPACE}"
-    kubectl apply -n "${ARGOCD_NAMESPACE}" -f https://raw.githubusercontent.com/argoproj/argo-cd/${version}/manifests/install.yaml
+    kubectl apply -n "${ARGOCD_NAMESPACE}" -f "https://raw.githubusercontent.com/argoproj/argo-cd/${version}/manifests/install.yaml"
 }
 
 function helm_install() {
     local chart_name=$1
 
-    pushd "${CHARTS_DIR}/core/${chart_name}" > /dev/null
-    help dependency build
-    helm upgrade --install . \
+    pushd "${CHARTS_DIR}/core/${chart_name}" > /dev/null || exit 1
+    helm dependency build
+    helm upgrade --install "${chart_name}" . \
         --namespace "${ARGOCD_NAMESPACE}" \
-        --values "${CHARTS_DIR}/${CLOUD}/${ENV}/values.yaml" \
-        --values "${CHARTS_DIR}/${CLOUD}/${ENV}/values-${CLOUD}-${ENV}.yaml"
+        --values "values.yaml" \
+        --values "values-${CLOUD}-${ENV}.yaml"
     sleep 10
     echo_success "${chart_name} installed"
-    popd > /dev/null
+    popd > /dev/null || exit 1
 }
 
-function crds_install() {
-    kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
-    kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
-    kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
-    kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
-    kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
-    kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
-    kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
-    kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
-    echo_success "CRDs Prometheus Operator created"
-}
+# function crds_install() {
+#     kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml
+#     kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagers.yaml
+#     kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
+#     kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_probes.yaml
+#     kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_prometheuses.yaml
+#     kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_prometheusrules.yaml
+#     kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
+#     kubectl apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/${PROM_OPERATOR_VERSION}/example/prometheus-operator-crd/monitoring.coreos.com_thanosrulers.yaml
+#     echo_success "CRDs Prometheus Operator created"
+# }
 
 function argocd_helm() {
     # local cd_version=$1
     # local appset_version=$2
-    # local notifs_version=$3 
+    # local notifs_version=$3
 
     kubectl create namespace "${ARGOCD_NAMESPACE}"
     echo_success "Namespace ${ARGOCD_NAMESPACE} created"
@@ -98,7 +98,7 @@ function argocd_helm() {
     #     --namespace "${ARGOCD_NAMESPACE}" \
     #     --version "${cd_version}" \
     #     --values "${CHARTS_DIR}/${CLOUD}/${ENV}/argocd-values.yaml"
-    helm_install "argocd"
+    helm_install "argo-cd"
 
     # helm template argocd-applicationset "${ARGO_REPO_NAME}/argocd-applicationset" \
     #     --namespace "${ARGOCD_NAMESPACE}" \
@@ -107,7 +107,7 @@ function argocd_helm() {
     # echo_success "ArgoCD ApplicationSet installed"
     # sleep 10
     echo helm_install "argocd-applicationset"
-    
+
     # helm template argo-rollouts "${ARGO_REPO_NAME}/argo-rollouts" \
     #     --namespace "${ARGOCD_NAMESPACE}" \
     #     --version "${rollouts_version}" \
@@ -115,7 +115,7 @@ function argocd_helm() {
     # echo_success "Argo Rollouts installed"
     # sleep 10
     echo helm_install "argocd-rollouts"
-    
+
     # helm template argocd-notifications "${ARGO_REPO_NAME}/argocd-notifications" \
     #     --namespace "${ARGOCD_NAMESPACE}" \
     #     --version "${notifs_version}" \
@@ -123,7 +123,7 @@ function argocd_helm() {
     # echo_success "ArgoCD Notifications installed"
     # sleep 10
     echo helm_install "argocd-notifications"
-    
+
     # kustomize build gitops/argocd/bootstrap/${CLOUD}/${ENV} | kubectl apply -f -
     helm_install "infra"
 
