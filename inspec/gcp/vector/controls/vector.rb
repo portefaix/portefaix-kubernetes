b9ceb8d1ebc71f5ec2f0636b965a73c2e9dc60af
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-01-04 18:01:00 +0100

    Update: Rename GCP environment into Inspec tests
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- inspec/gcp/vector/controls/vector.rb
+++ inspec/gcp/vector/controls/vector.rb
@@ -68,7 +68,7 @@ control 'vector-2' do
 
     its('storage_class') { should eq "STANDARD" }
     its('labels') { should include(
-      'env'     => "prod",
+      'env'     => "dev",
       'service' => "vector",
       'made-by' => "terraform"
     )}
