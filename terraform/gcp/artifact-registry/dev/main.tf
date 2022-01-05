42d0c2393fa13ba68becc59d18b34b7e23afd999
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-01-05 06:19:48 +0100

    Update: clean provider version
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/gcp/artifact-registry/dev/main.tf
+++ terraform/gcp/artifact-registry/dev/main.tf
@@ -16,9 +16,9 @@ terraform {
   required_version = ">= 1.0.0"
 
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "< 4.0.0"
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.5.0"
     }
   }
 }
--- terraform/gcp/artifact-registry/dev/provider.tf
+++ terraform/gcp/artifact-registry/dev/provider.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-provider "google" {
+provider "google-beta" {
   project = var.project
   region  = var.region
 }
