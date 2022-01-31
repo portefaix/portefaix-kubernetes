30770fea4f500d7834d841192d20e9ee23f720f9
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-01-31 13:44:49 +0100

    AWS WAF Configuration (#1699)
    * Add: AWS WAF configuration
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Add: AWS WAF configuration
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Add: WAF module for Terraform Cloud
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Add: WAF module into Github Actions
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Fix: typo
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Fix: clean code
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Fix: clean code
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Fix: whitelist and blacklist rules
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Fix: indent
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Fix: disable some code
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Update: refactoring waf rules code
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Update: refactoring waf rules code
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Update: aws rules
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Update: aws rules
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Update: aws rules
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Update: aws rules
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Update: refactoring WAF rules
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Fix: check array length
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ terraform/aws/waf/modules/waf/custom.tf
@@ -0,0 +1,135 @@
+# Copyright (C) 2021 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "aws_wafv2_ip_set" "whitelist" {
+  name               = local.acl_whitelist_name
+  description        = var.description
+  scope              = var.scope
+  ip_address_version = "IPV4"
+  addresses          = var.whitelist_ipv4
+
+  tags = merge({
+    Name = local.acl_whitelist_name
+  }, var.tags)
+}
+
+resource "aws_wafv2_ip_set" "blacklist" {
+  name               = local.acl_blacklist_name
+  description        = var.description
+  scope              = var.scope
+  ip_address_version = "IPV4"
+  addresses          = var.blacklist_ipv4
+
+  tags = merge({
+    Name = local.acl_blacklist_name
+  }, var.tags)
+}
+
+resource "aws_wafv2_web_acl" "custom" {
+  count = length(concat(var.whitelist_ipv4, var.allowed_country_codes, var.blacklist_ipv4)) == 0 ? 0 : 1
+
+  name        = local.acl_custom_name
+  description = var.description
+  scope       = var.scope
+
+  default_action {
+    allow {}
+  }
+
+  dynamic "rule" {
+    for_each = var.whitelist_ipv4 == [] ? [] : [1]
+
+    content {
+      name     = local.rule_whitelist_ips
+      priority = 1
+
+      action {
+        allow {}
+      }
+
+      statement {
+        ip_set_reference_statement {
+          arn = aws_wafv2_ip_set.whitelist.arn
+        }
+      }
+
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = local.rule_whitelist_ips
+        sampled_requests_enabled   = true
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = var.allowed_country_codes == [] ? [] : [1]
+
+    content {
+      name     = local.rule_whitelist_country_name
+      priority = 2
+
+      action {
+        allow {}
+      }
+
+      statement {
+        geo_match_statement {
+          country_codes = var.allowed_country_codes
+        }
+      }
+
+      visibility_config {
+        cloudwatch_metrics_enabled = var.cloudwatch_metrics_enabled
+        metric_name                = local.rule_whitelist_country_name
+        sampled_requests_enabled   = true
+      }
+    }
+  }
+
+  dynamic "rule" {
+    for_each = var.blacklist_ipv4 == [] ? [] : [1]
+
+    content {
+      name     = local.rule_blacklist_ips
+      priority = 3
+
+      action {
+        block {}
+      }
+
+      statement {
+        ip_set_reference_statement {
+          arn = aws_wafv2_ip_set.blacklist.arn
+        }
+      }
+
+      visibility_config {
+        cloudwatch_metrics_enabled = true
+        metric_name                = local.rule_blacklist_ips
+        sampled_requests_enabled   = true
+      }
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = var.cloudwatch_metrics_enabled
+    metric_name                = local.acl_core_name
+    sampled_requests_enabled   = true
+  }
+
+  tags = merge({
+    Name = local.acl_custom_name
+  }, var.tags)
+
+}
---
+++ terraform/aws/waf/staging/README.md
@@ -0,0 +1,42 @@
+## Documentation
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.0.0 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | 3.73.0 |
+
+## Providers
+
+No providers.
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_waf"></a> [waf](#module\_waf) | ../modules/waf | n/a |
+
+## Resources
+
+No resources.
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_allowed_country_codes"></a> [allowed\_country\_codes](#input\_allowed\_country\_codes) | Whitelist access by country | `list(string)` | `[]` | no |
+| <a name="input_blacklist_ipv4"></a> [blacklist\_ipv4](#input\_blacklist\_ipv4) | Block blacklist for IPV4 addresses | `list(string)` | `[]` | no |
+| <a name="input_cloudwatch_metrics_enabled"></a> [cloudwatch\_metrics\_enabled](#input\_cloudwatch\_metrics\_enabled) | Indicating whether the associated resource sends metrics to CloudWatch | `bool` | `false` | no |
+| <a name="input_default_tags"></a> [default\_tags](#input\_default\_tags) | Tags for the AWS provider | `map(string)` | <pre>{<br>  "Made-By": "terraform",<br>  "Project": "portefaix"<br>}</pre> | no |
+| <a name="input_region"></a> [region](#input\_region) | AWS Region | `string` | n/a | yes |
+| <a name="input_scope"></a> [scope](#input\_scope) | Specifies whether this is for an AWS CloudFront distribution or for a regional application | `string` | `"REGIONAL"` | no |
+| <a name="input_service_name"></a> [service\_name](#input\_service\_name) | Name of the service | `string` | n/a | yes |
+| <a name="input_tags"></a> [tags](#input\_tags) | Tags for the WAF resources | `map(string)` | <pre>{<br>  "made-by": "terraform"<br>}</pre> | no |
+| <a name="input_whitelist_ipv4"></a> [whitelist\_ipv4](#input\_whitelist\_ipv4) | Allow whitelist for IPV4 addresses | `list(string)` | `[]` | no |
+
+## Outputs
+
+No outputs.
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
