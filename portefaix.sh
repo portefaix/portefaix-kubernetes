84751c8e3b7c484e353fa40c891c1888bf6c76cc
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-03-03 12:36:03 +0000

    IBMCloud infrastructure (#1913)
    * Add: IBMCloud CLI resources
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Add: Terraform code for IKS
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>
    
    * Add: Makefile goals for IBMCloud and IKS
    
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- portefaix.sh
+++ portefaix.sh
@@ -18,22 +18,28 @@ OK_COLOR="\e[32m"
 KO_COLOR="\e[31m"
 NO_COLOR="\e[39m"
 
+reset_color="\\e[0m"
+color_red="\\e[31m"
+color_green="\\e[32m"
+color_blue="\\e[36m";
+
+function echo_fail { echo -e "${color_red}✖ $*${reset_color}"; }
+function echo_success { echo -e "${color_green}✔ $*${reset_color}"; }
+function echo_info { echo -e "${color_blue}$*${reset_color}"; }
+
 echo -e "${OK_COLOR}[ Portefaix ]${NO_COLOR}"
 
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <cloud provider>"
 else
-    echo -e "${OK_COLOR}Setup credentials ${NO_COLOR}"
     CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
     PORTEFAIX_CONFIG_HOME=${CONFIG_HOME}/portefaix
-
-    export CREDENTIALS=${PORTEFAIX_CONFIG_HOME}/portefaix.sh
-
+    CREDENTIALS=${PORTEFAIX_CONFIG_HOME}/portefaix.sh
     if [ ! -f "${CREDENTIALS}" ]; then
-        echo -e "${KO_COLOR}File not found: ${NO_COLOR}${CREDENTIALS}"
+        echo_fail "File not found: ${CREDENTIALS}"
     else
+        echo_success "Configuration file"
         # shellcheck disable=SC1090
         . "${CREDENTIALS}" "$1"
-        echo -e "${OK_COLOR}Done${NO_COLOR}"
     fi
 fi
