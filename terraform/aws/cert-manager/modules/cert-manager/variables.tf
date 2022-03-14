bc790ce46e247610e8db08467a051366298c7fed
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-03-14 16:21:05 +0100

    Fix: typo
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/aws/cert-manager/modules/cert-manager/variables.tf
+++ terraform/aws/cert-manager/modules/cert-manager/variables.tf
@@ -32,7 +32,7 @@ variable "service_account" {
 
 variable "tags" {
   type        = map(string)
-  description = "Tags for VPC"
+  description = "Tags for AWS resources"
   default = {
     "made-by" = "terraform"
   }
