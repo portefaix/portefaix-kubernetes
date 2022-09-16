908e9213463068d6f4f424b8052ffd0a7203ea28
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-09-16 16:05:23 +0200

    Add: IAM users and groups
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

---
+++ terraform/aws/root/terraform/iam_users.tf
@@ -0,0 +1,32 @@
+# Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "aws_iam_user" "nicolas_lamirault" {
+  name          = "NicolasLamirault"
+  path          = "/"
+  force_destroy = true
+
+  tags = merge(
+    { "Name" = "NicolasLamirault" },
+    var.tags
+  )
+}
+
+resource "aws_iam_user_group_membership" "nicolas_lamirault" {
+  user = aws_iam_user.nicolas_lamirault.name
+  groups = [
+    # aws_iam_group.enforce_mfa.name,
+    var.admin_group_name
+  ]
+}
