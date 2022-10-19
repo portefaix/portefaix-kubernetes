3b6f68f0508f9919f04bbda5ed3b9e4de55db5fd
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-10-19 07:59:24 +0200

    Fix: missing variable
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- terraform/azure/vnet/modules/vnet/vnet.tf
+++ terraform/azure/vnet/modules/vnet/vnet.tf
@@ -17,6 +17,7 @@ module "vnet" {
   version = "3.0.0"
 
   vnet_name           = var.vnet_name
+  vnet_location       = var.resource_group_location
   resource_group_name = azurerm_resource_group.vnet.name
 
   address_space   = var.address_space
