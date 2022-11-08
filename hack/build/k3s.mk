5e5f1dcf500833e3719e8afcf4571db83d136019
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2022-11-08 22:14:46 +0100

    Fix: Kube-proxy monitoring
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- hack/build/k3s.mk
+++ hack/build/k3s.mk
@@ -38,9 +38,10 @@ K3S_VERSION = $(K3S_VERSION_$(ENV))
 K3S_USER    = $(K3S_USER_$(ENV))
 
 K3S_ARGS := --disable metrics-server --no-deploy=traefik --disable traefik
-K3S_ARGS += --kube-controller-manager-arg 'bind-address=0.0.0.0' --kube-controller-manager-arg 'bind-address=0.0.0.0'
-K3S_ARGS += --kube-proxy-arg 'bind-address=0.0.0.0'
-K3S_ARGS += --kube-scheduler-arg 'bind-address=0.0.0.0' --kube-scheduler-arg 'bind-address=0.0.0.0'
+K3S_ARGS += --kube-controller-manager-arg 'address=0.0.0.0' --kube-controller-manager-arg 'bind-address=0.0.0.0'
+K3S_ARGS += --kube-scheduler-arg 'address=0.0.0.0' --kube-scheduler-arg 'bind-address=0.0.0.0'
+K3S_ARGS += --kube-proxy-arg 'metrics-bind-address=0.0.0.0' --kube-proxy-arg 'bind-address=0.0.0.0'
+# K3S_ARGS += --etcd-expose-metrics true
 
 AKEYLESS_PROFILE = $(AKEYLESS_PROFILE_$(ENV))
 
