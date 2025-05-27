e3b757e654dd0fd6157ec9a927c0c715ea4df996
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2025-05-27 17:23:39 +0200

    feat: cleanup
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- portefaix.sh
+++ portefaix.sh
@@ -11,35 +11,37 @@ reset_color="\\e[0m"
 color_red="\\e[31m"
 color_green="\\e[32m"
 color_blue="\\e[36m"
+color_yellow="\\e[33m"
 
-function echo_fail { echo -e "${color_red}✖ $*${reset_color}"; }
-function echo_success { echo -e "${color_green}✔ $*${reset_color}"; }
-function echo_info { echo -e "${color_blue}$*${reset_color}"; }
+function echo_fail { echo -e "${color_red}🔴 $*${reset_color}"; }
+function echo_success { echo -e "${color_green}✅ $*${reset_color}"; }
+function echo_info { echo -e "${color_blue}🔵 $*${reset_color}"; }
+function echo_warn { echo -e "${color_yellow}🟡 $*${reset_color}"; }
 
 echo -e "${OK_COLOR}🏗  Configure Portefaix${NO_COLOR}"
 
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 <cloud provider>"
+  echo "Usage: $0 <cloud provider>"
 else
-    CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
-    PORTEFAIX_CONFIG_HOME=${CONFIG_HOME}/portefaix
-    CREDENTIALS=${PORTEFAIX_CONFIG_HOME}/portefaix.sh
-    if [ ! -f "${CREDENTIALS}" ]; then
-        echo_fail "File not found: ${CREDENTIALS}"
+  CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
+  PORTEFAIX_CONFIG_HOME=${CONFIG_HOME}/portefaix
+  CREDENTIALS=${PORTEFAIX_CONFIG_HOME}/portefaix.sh
+  if [ ! -f "${CREDENTIALS}" ]; then
+    echo_fail "File not found: ${CREDENTIALS}"
+  else
+    # shellcheck disable=SC1090
+    echo -e "${OK_COLOR}📝 Load environement variables${NO_COLOR}"
+    # shellcheck disable=SC1090
+    source "${CREDENTIALS}" "$1"
+
+    echo_info "Terraform variables"
+    export TF_VAR_portefaix_version="v0.43.0"
+
+    # shellcheck disable=SC2181
+    if [ $? -eq 0 ]; then
+      echo -e "${OK_COLOR}🎉 Congratulations ! Portefaix is configured${NO_COLOR}"
     else
-        # shellcheck disable=SC1090
-        echo -e "${OK_COLOR}📝 Load environement variables${NO_COLOR}"
-        # shellcheck disable=SC1090
-        source "${CREDENTIALS}" "$1"
-
-        echo_info "Terraform variables"
-        export TF_VAR_portefaix_version="v0.43.0"
-
-        # shellcheck disable=SC2181
-        if [ $? -eq 0 ]; then
-            echo -e "${OK_COLOR}🎉 Congratulations ! Portefaix is configured${NO_COLOR}"
-        else
-            echo_fail "Failed"
-        fi
+      echo_fail "Failed"
     fi
+  fi
 fi
